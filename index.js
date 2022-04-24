935a4dbe3536a06e43ee0cbde6aae22bd0ab9f93
nurabdu <abdurashitov.beknur@gmail.com>
2022-04-24 14:16:27 +0300

    navlink

---
+++ index.js
@@ -0,0 +1,19 @@
+// function checkAge(age) {
+//     if (age > 18){
+//         return true
+//     } else {
+//         return confirm("Stop!");
+//     }
+// }
+
+// let age = prompt('how old to you', 18);
+
+// if (checkAge(age)) {
+//     alert('connect');s
+// } else {
+//     alert('not connect');
+// }
+
+// function Click() {
+//     onclick()
+// }
